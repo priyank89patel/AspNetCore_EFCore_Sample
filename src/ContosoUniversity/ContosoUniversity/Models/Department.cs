27fd8904ea910b5d1431a9ce737eebade4dc2fbb
Priyank Patel <priyank89patel@gmail.com>
2020-04-27 21:33:33 -0400

    EF handle concurrency conflicts

--- src/ContosoUniversity/ContosoUniversity/Models/Department.cs
+++ src/ContosoUniversity/ContosoUniversity/Models/Department.cs
@@ -22,8 +22,11 @@ namespace ContosoUniversity.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name ="Start Date")]
         public DateTime StartDate { get; set; }
-
+        
         public int? InstructorID { get; set; }
+        
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
